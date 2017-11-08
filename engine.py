c8673f637fffb2070d9d21ed2f671acf3e6ac4b4
Thomas Van Voorhis <iqeeyt+edx@gmail.com>
2017-11-08 14:12:12 +0000

    Did stufffffffff
    I jhava to push this real quick so byeeeeeee

--- engine.py
+++ engine.py
@@ -1,4 +1,6 @@
-class Point:
+import math
+
+class Vector:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
@@ -6,16 +8,32 @@ class Point:
     def __str__(self):
         return "({}, {})".format(self.x, self.y)
 
-class Vector:
-    def __init__(self, pos=Point(), vel=Point()):
-        self.pos = pos
-        self.vel = vel
+    def length(self):
+        return math.sqrt(self.x**2, self.y**2)
 
-    def __str__(self):
-        return "({}+{}, {}+{})".format(x,vx,y,vy)
+class Thing(Vector):
+    def __init__(self, x=0, y=0, vx=0, vy=0, mass=1, symbol='O')
+        # invoke super class
+        super(x,y)
+        # setup other properties
+        self.vx = vx
+        self.vy = vy
+        self.mass = mass
+        self.object = object
+
+    def np(self):
+        return Vector(self.x+self.vx, self.y+self.vy)
 
 class Space:
-    def __init__(self, dimensions=Point(), friction=1):
-        self.width = dimensions.x
-        self.height = dimensions.y
+    def __init__(self, width=16, height=16, friction=1, gravity=1):
+        # define some basic spacial parameters
+        self.width = width
+        self.height = height
         self.friction = friction
+        self.gravity = gravity
+        self.things = []
+
+    def add(self, thing=Thing()):
+        self.things += thing
+
+    def update(self):
