1d1e549fe68e82aca81ed613cb99556f48e1e09f
Thomas Van Voorhis <iqeeyt+edx@gmail.com>
2017-11-13 17:34:59 +0000

    Added interactive prompt and made some tweaks to the physics * Interactive prompt (Space.prompt()) * Added __str__ magic method for Thing * Added "verbose" draw which draws all of the objects in space with all of their properies

--- engine.py
+++ engine.py
@@ -12,18 +12,22 @@ class Vector:
         return math.sqrt(self.x**2, self.y**2)
 
 class Thing:
-    def __init__(self, vel=Vector(), pos=Vector(), mass=0, symbol='.')
+    def __init__(self, vel=Vector(), pos=Vector(), mass=0, symbol='.'):
         # setup other properties
         self.vel = vel
         self.pos = pos
         self.mass = mass
         self.object = object
+        self.symbol = symbol
+
+    def __str__(self):
+        return "[({},{})({},{}),{},{}]".format(self.pos.x, self.pos.y, self.vel.x, self.vel.y, self.mass, self.symbol)
 
     def velocity(self):
         return self.vel
 
 class Space:
-    def __init__(self, width=16, height=16, friction=1, gravity=1):
+    def __init__(self, width=16, height=8, friction=1, gravity=1):
         # define some basic spatial parameters
         self.width = width
         self.height = height
@@ -49,26 +53,57 @@ class Space:
             return c
 
     def update(self):
+        """Update all of the objects in space"""
         # loop through all of the objects in space
-        for x in range(len(space)):
-            for y in range(len(space[x])):
+        for x in range(len(self.space)):
+            for y in range(len(self.space[x])):
                 # save the object at this location
-                thing = space[x][y]
+                thing = self.space[x][y]
                 # write an empty object to the empty space
-                space[x][y] = Thing()
+                self.space[x][y] = Thing()
                 # update the object's coords
-                thing.pos.x = clamp(width, thing.vel.x+x)
-                thing.pos.y = clamp(height, thing.vel.y+y)
+                thing.pos.x = self.clamp(self.width, thing.vel.x+thing.pos.x)
+                thing.pos.y = self.clamp(self.height, thing.vel.y+thing.pos.y)
                 # write the new coords to the space
-                space[thing.x][thing.y] = thing
+                self.space[thing.pos.x][thing.pos.y] = thing
                 # update the velocity
                 thing.vel.x -= self.friction
                 thing.vel.y -= self.gravity
 
     def draw(self):
-        for x in range(len(space)):
-            for y in range(len(space[x])):
-                print(space[x][y].symbol)
+        """Draw space"""
+        for x in range(len(self.space)):
+            for y in range(len(self.space[x])):
+                print(self.space[x][y].symbol,end='')
             print("\n")
 
+    def vdraw(self):
+        """Draw space with all information of the blocks"""
+        for x in range(len(self.space)):
+            for y in range(len(self.space[x])):
+                print(self.space[x][y],end='')
+            print("\n")
+
+    def prompt(self):
+        """Setup interactive prompt for manipulating physical space"""
+        cmd = ""
+        while cmd != "quit":
+            cmd = input(">")
+            if cmd == "draw":
+                print("\n" * 100)
+                self.draw()
+            elif cmd == "update":
+                self.update()
+            elif cmd == "add":
+                self.add(Thing(Vector(int(input("X Position? ")), int(input("Y Position? "))), Vector(int(input("X Velocity? ")), int(input("Y Velocity? "))), int(input("Mass? ")), input("Symbol? ")[0]))
+            elif cmd == "remove":
+                self.remove(input("X Position? "), input("Y Position? "))
+            elif cmd == "obj":
+                self.vdraw()
+            else:
+                print("unrecognized command")
+        print("\n" * 100)
+        print("Thank you for using physiics")
+
 space = Space()
+space.prompt()
