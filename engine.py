6e8cbf211129199bed05f085aa6d87f01171c217
Thomas Van Voorhis <iqeeyt+edx@gmail.com>
2017-11-14 17:48:09 +0000

    Did stuf Cleaned up prompt Added a long-term run command ? Lost the caps for gravity and friction. sad :( did some confusing bubber shtuff UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import math
+from time import sleep
 
 class Vector:
     def __init__(self, x=0, y=0):
@@ -21,13 +22,13 @@ class Thing:
         self.symbol = symbol
 
     def __str__(self):
-        return "[({},{})({},{}),{},{}]".format(self.pos.x, self.pos.y, self.vel.x, self.vel.y, self.mass, self.symbol)
+        return "[({},{})({},{}),{},{}]".format(self.pos.x,self.pos.y,self.vel.x,self.vel.y,self.mass,self.symbol)
 
     def velocity(self):
         return self.vel
 
 class Space:
-    def __init__(self, width=16, height=8, friction=1, gravity=1):
+    def __init__(self, width=48, height=16, friction=1, gravity=1):
         # define some basic spatial parameters
         self.width = width
         self.height = height
@@ -72,37 +73,59 @@ class Space:
 
     def draw(self):
         """Draw space"""
+        # clear the screen
+        self.clear()
+        # loop through all of the objects in space and print them
         for x in range(len(self.space)):
             for y in range(len(self.space[x])):
+                # print the object's symbol
                 print(self.space[x][y].symbol,end='')
-            print("\n")
+            print("")
 
     def vdraw(self):
         """Draw space with all information of the blocks"""
+        self.clear()
         for x in range(len(self.space)):
             for y in range(len(self.space[x])):
+                # print the object
                 print(self.space[x][y],end='')
             print("\n")
 
-    def prompt(self):
-        """Setup interactive prompt for manipulating physical space"""
-        cmd = ""
-        while cmd != "quit":
-            cmd = input(">")
-            if cmd == "draw":
-                print("\n" * 100)
-                self.draw()
-            elif cmd == "update":
-                self.update()
-            elif cmd == "add":
-                self.add(Thing(Vector(int(input("X Position? ")), int(input("Y Position? "))), Vector(int(input("X Velocity? ")), int(input("Y Velocity? "))), int(input("Mass? ")), input("Symbol? ")[0]))
-            elif cmd == "remove":
-                self.remove(input("X Position? "), input("Y Position? "))
-            elif cmd == "obj":
-                self.vdraw()
-            else:
-                print("unrecognized command")
+    def clear(self):
+        """Clear the screen"""
         print("\n" * 100)
 
-space = Space()
-space.prompt()
+    def run(self, interval=1, length=1):
+        for i in range(length):
+            self.update()
+            self.draw()
+            sleep(interval)
+
+    def prompt(self):
+        """Setup interactive prompt for manipulating physical space"""
+        self.clear()
+        # Make some wrappers for functions that require input
+        def prompt_add():
+            self.add(Thing(Vector(int(input("X Position? ")),int(input("Y Position? "))),Vector(int(input("X Velocity? ")),int(input("Y Velocity? "))),int(input("Mass? ")),input("Symbol? ")[0]))
+        def prompt_remove():
+            self.remove(int(input("X Position? ")), int(input("Y Position? ")))
+        def prompt_run():
+            self.run(float(input("Interval? ")),int(input("Iterations? ")))
+        cmd = ""
+        # commands used by the prompt
+        cmds = {
+            "draw": self.draw,
+            "update": self.update,
+            "add": prompt_add,
+            "remove": prompt_remove,
+            "vdraw": self.vdraw,
+            "run": prompt_run
+        }
+        while cmd != "quit" and cmd != "q":
+            cmd = input(">")
+            try:
+                self.clear()
+                cmds[cmd]()
+            except:
+                print("Unrecognized command")
+        self.clear()
