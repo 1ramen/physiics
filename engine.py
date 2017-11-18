87c9672cc6011de0e8edaa792b802a88a2fc4673
Thomas Van Voorhis <iqeeyt+edx@gmail.com>
2017-11-18 20:55:46 +0000

    General refactoring Readded the prompt Added an IO class for prettier output Added removal function (basically a wrapper for add the just places an empty Thing)

--- engine.py
+++ engine.py
@@ -1,3 +1,6 @@
+""" Engine for caluculating integer based physics """
+
+from termcolor import colored
 import math
 
 class Physics:
@@ -18,6 +21,17 @@ class Physics:
             return int(maxi)
         return int(value)
 
+class IO:
+    """Static class for handling IO"""
+    @staticmethod
+    def clear():
+        """Clear the screen"""
+        print("\n" * 100)
+
+    @staticmethod
+    def cprint(text, color):
+        """Print colored text"""
+        print(colored(text, color))
 
 class Vector:
     """XY pair for calculating motion"""
@@ -28,7 +42,15 @@ class Vector:
         self.y = int(y)
 
     def length(self):
+        """Return vector length"""
+        # use pythagorean theorem to calculate the overall length
         return math.sqrt(self.x ** 2, self.y ** 2)
+
+    def reset(self):
+        """Clear the Vector"""
+        # set the vector to a default value and return it
+        self = Vector()
+        return self
 
     def __str__(self):
         return "({}, {})".format(self.x, self.y)
@@ -49,8 +71,10 @@ class Vector:
         return Vector(self.x // other.x, self.y // other.y)
 
     def __iadd__(self, other):
-        self.x += other.x
-        self.y += other.y
+        self = self + other
+
+    def __isub__(self, other):
+        self = self - other
 
     def __neg__(self):
         return Vector(0 - self.x, 0 - self.y)
@@ -59,7 +83,7 @@ class Vector:
         return Vector(self.y, self.x)
 
 
-class Obj:
+class Thing:
     """Object in space"""
     def __init__(self, velocity=Vector(), mass=0, symbol='.'):
         self.velocity = velocity
@@ -77,12 +101,12 @@ class Obj:
 
         # pull value to zero
         if self.velocity.x > 0:
-            self.velocity.x = Physics.clamp(self.velocity.x + int(acceleration.x), mini=0)
-            self.velocity.y = Physics.clamp(self.velocity.y + int(acceleration.y), mini=0)
+            self.velocity.x = Physics.clamp(self.velocity.x + acceleration.x, mini=0)
+            self.velocity.y = Physics.clamp(self.velocity.y + acceleration.y, mini=0)
             return
         elif self.velocity.x < 0:
-            self.velocity.x = Physics.clamp(self.velocity.x - int(acceleration.x), maxi=0)
-            self.velocity.y = Physics.clamp(self.velocity.y - int(acceleration.y), maxi=0)
+            self.velocity.x = Physics.clamp(self.velocity.x - acceleration.x, maxi=0)
+            self.velocity.y = Physics.clamp(self.velocity.y - acceleration.y, maxi=0)
             return
 
         # return 0 because the value cannot go up or down
@@ -91,8 +115,89 @@ class Obj:
     def update(self, drag=0, friction=Vector(), gravity=0):
         """Update the object's velocity based on environment values"""
         # increase friction with air drag
-        friction.move(drag)
+        friction += Vector(drag)
 
         # apply gravity and friction
         self.velocity.y -= int(gravity)
-        self.acc(Vector(friction.x, friction.y).negate())
+        self.acc(-friction)
+
+class Space:
+    """Calculate spacial relations"""
+    def __init__(self, dimensions=Vector(), friction=1, drag=1, gravity=1):
+        self.dimensions = dimensions
+        self.friction = friction
+        self.drag = drag
+        self.gravity = gravity
+        self.space = [[Thing() for i in range(self.dimensions.y)] for j in range(self.dimensions.x)]
+
+    def add(self, thing=Thing(), pos=Vector()):
+        """Add a Thing to space"""
+        self.space[pos.x][pos.y] = thing
+        return pos
+        # note: this will overwrite anything currently existing at the given location
+
+    def remove(self, pos=Vector()):
+        """Remove anything at a given position"""
+        self.space[pos.x][pos.y] = Thing()
+        return pos
+
+    def prompt(self):
+        """Run interactive prompt for the physics engine"""
+
+        # define I/O versions of all of the methods
+        def add():
+            """Add object based on input"""
+            try:
+                # create parameters from user input for new object
+                pos = Vector(int(input("X position? ")), int(input("Y position? ")))
+                vel = Vector(int(input("X velocity? ")), int(input("Y velocity? ")))
+                mass = int(input("Mass? "))
+                symbol = input()[0]
+            except ValueError:
+                IO.cprint("invalid input", "red")
+                return None
+
+            # add the generated object to space
+            return self.add(Thing(vel, mass, symbol), pos)
+
+        def remove():
+            """Remove object based on user input"""
+            # get location from user
+            pos = Vector(int(input("X position? ")), int(input("Y position? ")))
+
+            # remove object @ given location
+            return self.remove(pos)
+
+        def quit():
+            """Placeholder function so no error is returned when quit is entered"""
+            return None
+
+        # define all of the commands
+        commands = {
+            "q":      quit,
+            "quit":   quit,
+            "exit":   quit,
+            "add":    add,
+            "a":      add,
+            "new":    add,
+            "n":      add,
+            "remove": remove,
+            "rem":    remove,
+            "clear":  remove,
+            "delete": remove,
+            "del":    remove,
+            "d":      remove,
+        }
+        command = ""
+
+        # loop and check for commands
+        while command not in ["quit", "q", "exit"]:
+            command = input("> ")
+            # execute the specified command
+            try:
+                IO.clear()
+                commands[command]()
+            except KeyError:
+                IO.clear()
+                IO.cprint("unrecognized command", "red")
+        IO.cprint("thanks for using physiics :)", "green")
